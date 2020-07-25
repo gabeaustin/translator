c9bc3b7c34a86d3b83edc26f6d7627b630c56999
Gabe Austin <gabriel.austin2019@gmail.com>
2020-07-24 21:00:42 -0400

    add submit button function in js file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-alert("This is a test to see if the script file is linked correctly.");
+// alert("This is a test to see if the script file is linked correctly.");
+
+// This is what happens when the Submit button is pressed
+function formSubmit() {
+	alert("Your form was submitted correctly.")
+}
+
+// This is the data from the form
+function formData() {
+	document.getElementById("form_main").submit();
+
+}
 
