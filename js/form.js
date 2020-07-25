9472e337598b18ebc1dd4b4702bab28f112783c0
Gabe Austin <gabriel.austin2019@gmail.com>
2020-07-24 22:21:49 -0400

    add success and error functions

--- js/form.js
+++ js/form.js
@@ -2,6 +2,22 @@
 
 let form = document.getElementById("form_main");
 
+// This is for when users input no data or invalid data
+function error(english_word, spanish_word) {
+	// show the error msg
+	const error = input.previousElementSibling;
+	error.innerText = message;
+	return false;
+}
+
+// This is for when users input valid data
+function success(input) {
+	// hide the error msg
+	const error = input.previousElementSibling;
+	error.innerText = "";
+	return true;
+}
+
 // This is what happens when the Submit button is pressed
 function formSubmit() {
 	alert("Your form was submitted correctly.");
