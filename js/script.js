6abc77e31f66058a8ed2f0939ddb9c8b94835a85
Gabe Austin <gabriel.austin2019@gmail.com>
2020-07-24 21:24:36 -0400

    add js form functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 // alert("This is a test to see if the script file is linked correctly.");
 
+let form = document.getElementById("form_main");
+
 // This is what happens when the Submit button is pressed
 function formSubmit() {
-	alert("Your form was submitted correctly.")
+	alert("Your form was submitted correctly.");
 }
 
 // This is the data from the form
 function formData() {
-	document.getElementById("form_main").submit();
+	document.getElementById("form_main").focus().submit();
 
 }
 
